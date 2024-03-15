5bfb5a770f95d01f28b43a6ed304741769a403f6
Shariar Islam <shariarn85@gmail.com>
2024-03-15 23:54:49 +0600

    creating The Extra Reducers
    Creating the extrat reducers to create user use redux

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -22,7 +22,27 @@ const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {},
-  
+  extraReducers: builder => {
+    builder.addCase(createUser.pending, (state) => {
+      state.isLoading = true;
+      state.isError = false;
+      state.email = "";
+      state.name = "";
+      state.error = '';
+    }).addCase(createUser.fulfilled, (state, { payload }) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.email = payload.email;
+      state.name = payload.name;
+      state.error = '';
+    }).addCase(createUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.email = "";
+      state.name = "";
+      state.error = action.error.message;
+    })
+  }
 });
 
 export default userSlice.reducer;
