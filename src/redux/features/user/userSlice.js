754ec6333e5ace1a1f70aa123525ed9a79e90634
Shariar Islam <shariarn85@gmail.com>
2024-03-16 07:24:26 +0600

    initial stage in User Signup using redux
    Facing issue when calling firesebase create user with email and password
    to the userSlice

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import auth from '../../../utils/firebase.config';
 
 const initialState = {
@@ -10,11 +10,20 @@ const initialState = {
   error: '',
 };
 
-export const createUser = createAsyncThunk('userSlice/createUser', async ({ email, password }) => {
-  const data = await createUserWithEmailAndPassword(auth, email, password)
-  console.log(data);
-  return data
-})
+export const createUser = createAsyncThunk('userSlice/createUser',
+  async ({ email, password, name }) => {
+    console.log(email, password, name);
+
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName: name
+    })
+    console.log(data);
+    return {
+      email: data.user.email,
+      name: data.user.displayName,
+    };
+  })
 
 
 
@@ -22,26 +31,30 @@ const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {},
-  extraReducers: builder => {
-    builder.addCase(createUser.pending, (state) => {
-      state.isLoading = true;
-      state.isError = false;
-      state.email = "";
-      state.name = "";
-      state.error = '';
-    }).addCase(createUser.fulfilled, (state, { payload }) => {
-      state.isLoading = false;
-      state.isError = false;
-      state.email = payload.email;
-      state.name = payload.name;
-      state.error = '';
-    }).addCase(createUser.rejected, (state, action) => {
-      state.isLoading = false;
-      state.isError = false;
-      state.email = "";
-      state.name = "";
-      state.error = action.error.message;
-    })
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.email = "";
+        state.name = "";
+        state.error = '';
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        console.log(payload);
+        state.isLoading = false;
+        state.isError = false;
+        state.email = payload.email;
+        state.name = payload.name;
+        state.error = '';
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.email = "";
+        state.name = "";
+        state.error = action.error.message;
+      })
   }
 });
 
