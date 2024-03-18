a70440e9199fd980e65e6762244074203c4ea191
Shariar Islam <shariarn85@gmail.com>
2024-03-18 07:03:15 +0600

    Creating Base Api Layer

---
+++ src/redux/features/api/baseApi.js
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+const baseApi = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    endpoints: builder => ({
+        getTask: builder.query({
+            query: () => '/task'
+        })
+    })
+})
+
+export default baseApi;
